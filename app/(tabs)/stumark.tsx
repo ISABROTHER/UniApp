f5c7bdf36b27a4dcabab6fee8d7b71a68ffe3dca
ISABROTHER <ishimpatrickjohnston@gmail.com>
2026-02-27 19:11:57 +0100

    Updated stumark.tsx

--- app/(tabs)/stumark.tsx
+++ app/(tabs)/stumark.tsx
@@ -355,7 +355,6 @@ export default function StuMarkScreen() {
             <Text style={styles.bannerSubtitle}>Created by students and for students, this is an open platform for anyone to post what they want to sell, buy or trade.</Text>
           </View>
           <View style={styles.bannerBadge}>
-            <TrendingUp size={16} color={COLORS.white} />
           </View>
         </View>
 
