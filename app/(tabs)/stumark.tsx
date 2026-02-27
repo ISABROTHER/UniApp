6de22f838a2345d4b4a9438d48d5c448602c3ea9
ISABROTHER <ishimpatrickjohnston@gmail.com>
2026-02-27 19:12:25 +0100

    Updated stumark.tsx

--- app/(tabs)/stumark.tsx
+++ app/(tabs)/stumark.tsx
@@ -352,7 +352,7 @@ export default function StuMarkScreen() {
         <View style={styles.heroBanner}>
           <View style={styles.bannerContent}>
             <Text style={styles.bannerTitle}>StuMark (Student Market </Text>
-            <Text style={styles.bannerSubtitle}>Created by students and for students, this is an open platform for anyone to post what they want to sell, buy or trade.</Text>          </View>
+            <Text style={styles.bannerSubtitle}>Created by students and for students, this is an open platform for anyone to post what they want to sell, buy or trade.</Text>
         </View>
 
         <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.categories}>
