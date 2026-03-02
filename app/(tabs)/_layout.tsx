b9011bb9f2f4a255da7beec5b324d16d6ac18d3c
ISABROTHER <ishimpatrickjohnston@gmail.com>
2026-03-02 02:06:27 +0100

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Tabs } from 'expo-router';
 import { Platform } from 'react-native';
-import { Home, Search, MessageSquare, Ellipsis } from 'lucide-react-native';
+import { Home, Search, MessageSquare, EllipsisVertical } from 'lucide-react-native';
 import { COLORS, FONT } from '@/lib/constants';
 
 export default function TabLayout() {
@@ -50,7 +50,7 @@ export default function TabLayout() {
         name="profile"
         options={{
           title: 'More',
-          tabBarIcon: ({ color, size }) => <Ellipsis size={size} color={color} />,
+          tabBarIcon: ({ color, size }) => <EllipsisVertical size={size} color={color} />,
         }}
       />
       <Tabs.Screen
